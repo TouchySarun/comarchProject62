bdc5e24659a6b1347947bd30d1b90412f7c9f634
TANANPORN YANA <tananporn_yana@cmu.ac.th>
2019-10-27 20:20:19 +0700

    sw and beq

--- Simulator/i_type.py
+++ Simulator/i_type.py
@@ -6,8 +6,28 @@ def lw (code,g):
     offsetField = extract_binary(code,0,15)
 
     address = g["reg"][regA] + g["reg"][offsetField]
-    g["reg"] = g["mem"][address]
+    g["reg"][regB] = g["mem"][address]
     g["pc"] += 1
+
+
+def sw(code,g):
+    regA = extract_binary(code,19,21)
+    regB = extract_binary(code,16,18)
+    offsetField = extract_binary(code,0,15)
+
+    address = g["reg"][regA] + g["reg"][offsetField]
+    g["mem"][address] = g["reg"][regB]
+    g["pc"] += 1
+
+def beq(code,g):
+    regA = extract_binary(code,19,21)
+    regB = extract_binary(code,16,18)
+    offsetField = extract_binary(code,0,15)
+
+    if g["reg"][regA] == g["reg"][regB]:
+        g["pc"] += 1 + offsetField
+
+
 
 
 
